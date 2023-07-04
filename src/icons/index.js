fb8aa902078f4b49be4320c121f7ea9503773ff4
sunzhongli <2941913164@qq.com>
2023-07-04 15:56:15 +0800

    '第一次提交'

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+// 全局注册组件 Vue.component('组件名',组件)
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
